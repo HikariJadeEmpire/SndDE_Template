9985db46db379249b268b025c6c597abddde39b4
Hikari <118663358+HikariJadeEmpire@users.noreply.github.com>
2023-08-02 08:04:08 +0700

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -125,20 +125,20 @@ EOF
 }
 
 # EC2 budget constraint
-resource "aws_budgets_budget" "ec2" {
-  name              = "budget-ec2-monthly"
-  budget_type       = "COST"
-  limit_amount      = "5"
-  limit_unit        = "USD"
-  time_period_end   = "2087-06-15_00:00"
-  time_period_start = "2022-10-22_00:00"
-  time_unit         = "MONTHLY"
+# resource "aws_budgets_budget" "ec2" {
+#   name              = "budget-ec2-monthly"
+#   budget_type       = "COST"
+#   limit_amount      = "5"
+#   limit_unit        = "USD"
+#   time_period_end   = "2087-06-15_00:00"
+#   time_period_start = "2022-10-22_00:00"
+#   time_unit         = "MONTHLY"
 
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_email_addresses = [var.alert_email_id]
-  }
-}
+#   notification {
+#     comparison_operator        = "GREATER_THAN"
+#     threshold                  = 100
+#     threshold_type             = "PERCENTAGE"
+#     notification_type          = "FORECASTED"
+#     subscriber_email_addresses = [var.alert_email_id]
+#   }
+# }
